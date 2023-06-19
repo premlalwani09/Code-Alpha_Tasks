3f6539d3c6431cc093ed34609143ee1ecb485035
premlalwani09 <91013299+premlalwani09@users.noreply.github.com>
2023-06-19 22:24:34 +0530

    Add files via upload

---
+++ Background Generator/script.js
@@ -0,0 +1,29 @@
+let container = document.querySelector('.container');
+let button = document.querySelector('a');
+let displayHex = document.querySelector('p');
+
+const validcharacters = '0123456789ABCDEF';
+const hexDecimalLength = 6;
+
+
+function randomIntFromInterval(min,max){
+    return Math.floor(Math.random() * (max-min+1)+min);
+}
+
+
+function getNextRandomColor(){
+    let color ='#';
+    for (let i = 0; i < 6; i++) {
+        let getRandomChar = validcharacters.charAt(randomIntFromInterval(0,15));
+        color += getRandomChar;
+    };
+
+    displayHex.innerHTML = color;
+    container.style.backgroundColor = color;
+}
+
+getNextRandomColor();
+
+button.addEventListener('click',function(){
+    getNextRandomColor();
+},false);
